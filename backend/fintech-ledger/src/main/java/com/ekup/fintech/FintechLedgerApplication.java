50ddc1b18bd59d57dea734069633208f1b4fdec9
egemene <egemen.emreekici@tofas.com.tr>
2026-01-26 15:53:13 +0300

    Add backend and frontend source code files

---
+++ backend/fintech-ledger/src/main/java/com/ekup/fintech/FintechLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.ekup.fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechLedgerApplication.class, args);
+	}
+
+}
---
+++ backend/fintech-ledger/src/test/java/com/ekup/fintech/FintechLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekup.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
